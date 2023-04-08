29fec2ff9049e7706bacf2983538c48b3c01c29f
MephiboshethOfTheOrderOfSundays <128760212+MephiboshethOfTheOrderOfSundays@users.noreply.github.com>
2023-04-08 14:17:33 -0400

    Add files via upload

---
+++ class_instantiation.py
@@ -0,0 +1,8 @@
+from class_definition import do_math
+math_instance = do_math(3,4)
+print(math_instance.add())
+print(math_instance.subtract())
+print(math_instance.multiply())
+print(math_instance.divide())
+print(math_instance.double_input())
+print(math_instance.add())
